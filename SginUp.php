47a8a2257613751f3dbff38c29452a175e3ad420
Nada <nadoosh.2122002@gmail.com>
2022-07-26 09:14:19 +0300

    Add existing project

---
+++ SginUp.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Store</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <!-- Flaticons  -->
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<?php
+include "config/constants.php";
+if(isset($_POST['submit'])){
+    $username =$_POST['username'];
+    $password =md5($_POST['password']);
+    $phone =$_POST['phone'];
+    $email =$_POST['email'];
+    $address =$_POST['address'];
+    $sql="insert into users set username='$username',password='$password',phone='$phone',email='$email',address='$address'";
+    if ($username != null && $password != null && $phone!= null && $email!= null && $address!= null){
+        $res=mysqli_query($conn,$sql);
+        if($res){
+            $_SESSION['user']="<div class='success'>user is SginUp</div>";
+            header("location:signin.php");
+        }else{
+            $_SESSION['user']="<div class='error'>faild SginUp</div>";
+            header("location:pre_index.php");
+
+        }
+    }else{
+        $_SESSION['user']="<div class='error'>faild SginUp</div>";
+        header("location:pre_index.php");
+    }
+
+
+}
+
+
+
+?>
+
+
+    <aside id="colorlib-hero" class="breadcrumbs">
+        <div class="flexslider">
+            <ul class="slides">
+                <li style="background-image: url(images/cover-img-1.jpg);">
+                    <div class="overlay"></div>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+                                <div class="slider-text-inner text-center">
+                                    <h1>Sgin Up</h1>
+                                    <h2 class="bread"><span><a href="index.php">Home</a></span> <span>Sgin Up</span></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </aside>
+
+    <div class="colorlib-shop">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <form action="#" method="POST" class="colorlib-form">
+                        <h1>Sgin Up </h1>
+                        <div class="row" >
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <label for="fname">User Name </label>
+                                    <input type="text" id="fname" name="username" class="form-control" placeholder="Enter Your Name">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <label for="Phone">Phone Number</label>
+                                    <input type="text" id="zippostalcode" name="phone" class="form-control" placeholder="Enter Your Number">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <label for="email">E-mail </label>
+                                    <input type="text" id="email" class="form-control" name="email" placeholder="Enter Your Email">
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <input type="text" id="address" class="form-control" name="address" placeholder="Enter Your Address">
+                                </div>
+
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" id="password" class="form-control" name="password" placeholder="Enter Your Password">
+                                </div>
+
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <input type="submit" name="submit" value="Sgin Up" class="btn btn-primary" style="float: right;width: 162px;">
+                                </div>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php include"layouts/footer.php"?>
---
+++ fashion.sql
@@ -0,0 +1,197 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2022 at 07:48 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fashion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+(2, 'nada', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `title`, `image_name`) VALUES
+(5, 'MAN', '../images/category/MAN.jpg'),
+(6, 'KIDS', '../images/category/Kids.avif'),
+(7, 'WOMAN', '../images/category/WOMAN.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clothes`
+--
+
+CREATE TABLE `clothes` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `featured` varchar(255) NOT NULL,
+  `active` varchar(255) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `image_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clothes`
+--
+
+INSERT INTO `clothes` (`id`, `title`, `description`, `price`, `color`, `size`, `featured`, `active`, `cat_id`, `image_name`) VALUES
+(10, 'LOT Baby Rompers Short Sleeve 100%Cotton', 'Baby Rompers Short Sleeve\r\nAvailable in several colors, comfortable to wear\r\nIt can be worn on many occasions\r\nIt can be ordered now from our store in the right size and color', 15, 'white', 's', 'Yes', 'Yes', 6, '../images/clothes/1.avif'),
+(11, 'Long Sleeve Tops Floral Print Pants Headband', 'Long Sleeve Tops Floral Print Pants Headband\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 150, 'red', 's', 'Yes', 'Yes', 6, '../images/clothes/2.webp'),
+(12, ' Socks For Newborns', ' Socks For Newborns\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 50, 'Pink', 's', 'Yes', 'Yes', 6, '../images/clothes/3.webp'),
+(13, 'baby suit girl', 'baby suit girl\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 120, 'Pink', 's', 'Yes', 'Yes', 6, '../images/clothes/4.avif'),
+(16, 'Full outfit for winter', 'Full outfit for winter\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 400, 'gray', 'M', 'Yes', 'Yes', 7, '../images/clothes/7.jpeg'),
+(17, 'Full outfit for winter', 'Full outfit for winter Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 450, 'white', 'M', 'Yes', 'Yes', 7, '../images/clothes/8.jpeg'),
+(18, 'Casual wear for everyday wear	', 'Casual wear for everyday wear	 Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 500, 'Brouwn', 'M', 'Yes', 'Yes', 7, '../images/clothes/9.avif'),
+(19, 'Oversize Recycled Cotton Blend Cardigan', 'Oversize Recycled Cotton Blend Cardigan\r\n Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 350, 'gray', 'XL', 'Yes', 'Yes', 5, '../images/clothes/10.avif'),
+(20, 'casual suit', 'casual suit\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 250, 'white & gray', 'L', 'Yes', 'Yes', 5, '../images/clothes/11.jpeg'),
+(21, 'casual suit for every day user', 'casual suit\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 300, 'Brouwn & Red', 'L', 'Yes', 'Yes', 5, '../images/clothes/12.jpeg'),
+(22, 'Long Sleeve T-Shirt', 'Long Sleeve T-Shirt\r\nAvailable in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 170, 'white', 'XL', 'Yes', 'Yes', 5, '../images/clothes/13.avif'),
+(23, 'Jeans with a T-shirt', 'Jeans with a T-shirt\r\n Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 120, 'black', 's', 'Yes', 'Yes', 6, '../images/clothes/14.avif'),
+(24, 'Full outfit for summer', 'Full outfit for summer Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color	', 300, 'yellow', 'M', 'Yes', 'Yes', 7, '../images/clothes/15.jpeg'),
+(25, 'CASUAL SUIT', 'casual suit Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color ..\r\n\r\n', 500, 'brown', 'L', 'Yes', 'Yes', 5, '../images/clothes/14.jpg'),
+(26, 'BABY SUIT GIRL', 'baby suit girl Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color ..\r\n\r\n', 600, 'yellow', 'M', 'Yes', 'Yes', 6, '../images/clothes/15.jpg'),
+(27, 'FULL OUTFIT', 'Full outfit Available in several colors, comfortable to wear It can be worn on many occasions It can be ordered now from our store in the right size and color ..\r\n\r\n', 500, 'WHITE&LIGHT GREEN', 'L', 'Yes', 'Yes', 7, '../images/clothes/16.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `address`, `password`, `phone`, `email`) VALUES
+(1, 'nada', 'gaza', '202cb962ac59075b964b07152d234b70', 592212481, 'nada@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clothes`
+--
+ALTER TABLE `clothes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `clothes`
+--
+ALTER TABLE `clothes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `clothes`
+--
+ALTER TABLE `clothes`
+  ADD CONSTRAINT `clothes_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
